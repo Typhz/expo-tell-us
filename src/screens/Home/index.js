241bd78bd6317b3083b0b7c6c973081b145962f4
carlos3g <52337966+carlos3g@users.noreply.github.com>
2021-09-02 11:01:26 -0300

    [FEAT] screen finished: Home
    made according to https://www.figma.com/file/CaOASqZTY3KL0rh7Snjjxe/tell-us?node-id=1%3A3

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { Container, Text } from './styles';
+import { useNavigation } from '@react-navigation/native';
+import { Button } from '@components/';
+import { ActionButtons, Container, Content, Text, Title, U } from './styles';
 
 function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <Container>
-      <Text>Home</Text>
+      <Content>
+        <Text>Bem-vindo,</Text>
+        <Title>
+          Faça ou veja <U>confissões</U>.
+        </Title>
+      </Content>
+
+      <ActionButtons>
+        <Button
+          title="Fazer uma confissão"
+          style={{ marginBottom: 10 }}
+          onPress={() => navigate('MakeConfession')}
+          primary
+        />
+        <Button
+          title="Ver confissões aleatórias"
+          onPress={() => navigate('Confession')}
+        />
+      </ActionButtons>
     </Container>
   );
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,15 +1,36 @@
 import styled from 'styled-components/native';
+import { StatusBar } from 'react-native';
+import { tint } from 'polished';
 import { colors } from '@styles/';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
+  padding: ${StatusBar.currentHeight}px 16px 16px 16px;
   background-color: ${colors.background};
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  justify-content: flex-end;
 `;
 
 export const Text = styled.Text`
   font-family: RobotoSlab_400Regular;
   font-size: 16px;
+  color: ${tint(0.5, colors.text)};
+`;
+
+export const Title = styled.Text`
+  font-family: RobotoSlab_500Medium;
+  font-size: 44px;
   color: ${colors.text};
+`;
+
+export const U = styled.Text`
+  text-decoration: underline;
+`;
+
+export const ActionButtons = styled.View`
+  flex: 1;
+  justify-content: flex-end;
 `;
