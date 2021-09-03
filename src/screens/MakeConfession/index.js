6ff81180d0a4b98e201c9e06e7debd2bd72eab8b
carlos3g <52337966+carlos3g@users.noreply.github.com>
2021-09-03 14:44:00 -0300

    [FEAT] new screen: MakeConfession
    This screen is not completed since the API isn't made. Made according to https://www.figma.com/file/CaOASqZTY3KL0rh7Snjjxe/tell-us?node-id=8%3A6

--- src/screens/MakeConfession/index.js
+++ src/screens/MakeConfession/index.js
@@ -1,12 +1,96 @@
-import React from 'react';
-import { Container, Text } from './styles';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Button, Header, Input } from '@components/';
+import {
+  ActionButtons,
+  Content,
+  Inputs,
+  Text,
+  U,
+  Container,
+  Modal,
+  SuccessBox,
+  SuccessEmoji,
+  SuccessText,
+  ModalContent,
+} from './styles';
 
-function Home() {
+function MakeConfession() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [confession, setConfession] = useState('');
+  const [title, setTitle] = useState('');
+  const { goBack } = useNavigation();
+
+  const handlePublish = () => {
+    if (!confession) {
+      Alert.alert('Erro ao publicar', 'Nenhuma confissão foi escrita.');
+      return;
+    }
+
+    setIsModalVisible(true);
+  };
+
+  const handleDelete = () => {
+    setTitle('');
+    setConfession('');
+  };
+
   return (
     <Container>
-      <Text>MakeConfession</Text>
+      <Header onBackward={goBack} />
+
+      <Content>
+        <Text>
+          Confesse seus segredos mais profundos e sombrios de forma{' '}
+          <U>anônima</U>.
+        </Text>
+        <Inputs>
+          <Input
+            label="Título"
+            placeholder="Esfaqueei meu padrasto"
+            onChangeText={setTitle}
+            value={title}
+            style={{ marginBottom: 10 }}
+          />
+          <Input
+            label="Confissão"
+            placeholder="Quanto eu tinha 18 anos..."
+            onChangeText={setConfession}
+            value={confession}
+            textAlignVertical="top"
+            inputStyle={{ height: 300 }}
+            required
+          />
+        </Inputs>
+        <ActionButtons>
+          <Button
+            title="Excluir"
+            onPress={handleDelete}
+            style={{ width: '29%' }}
+          />
+          <Button
+            title="Publicar"
+            onPress={handlePublish}
+            style={{ width: '69%' }}
+            primary
+          />
+        </ActionButtons>
+      </Content>
+
+      <Modal
+        visible={isModalVisible}
+        onRequestClose={() => setIsModalVisible(false)}
+      >
+        <ModalContent onPress={() => setIsModalVisible(false)}>
+          <SuccessBox>
+            <SuccessEmoji>🎉</SuccessEmoji>
+            <SuccessText>Confissão feita com Sucesso</SuccessText>
+          </SuccessBox>
+        </ModalContent>
+      </Modal>
     </Container>
   );
 }
 
-export default Home;
+export default MakeConfession;
--- src/screens/MakeConfession/styles.js
+++ src/screens/MakeConfession/styles.js
@@ -3,13 +3,70 @@ import { colors } from '@styles/';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
   background-color: ${colors.background};
 `;
 
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    flexGrow: 1,
+    justifyContent: 'space-between',
+    padding: 16,
+    paddingTop: 0,
+  },
+  showsVerticalScrollIndicator: false,
+})``;
+
 export const Text = styled.Text`
   font-family: RobotoSlab_400Regular;
-  font-size: 16px;
+  font-size: 22px;
   color: ${colors.text};
+`;
+
+export const U = styled.Text`
+  text-decoration: underline;
+`;
+
+export const Inputs = styled.View`
+  margin: 20px 0;
+`;
+
+export const ActionButtons = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Modal = styled.Modal.attrs({
+  animationType: 'slide',
+  statusBarTranslucent: true,
+  transparent: true,
+})``;
+
+export const ModalContent = styled.Pressable`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SuccessBox = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  height: 250px;
+  background-color: ${colors.text};
+  border-radius: 37.5px;
+  elevation: 25;
+  padding: 0 10%;
+`;
+
+export const SuccessEmoji = styled.Text`
+  font-size: 78px;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const SuccessText = styled.Text`
+  font-family: RobotoSlab_400Regular;
+  font-size: 16px;
+  text-align: center;
+  color: ${colors.background};
 `;
