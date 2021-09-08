1b4570ca540bad4c6276d3ecc49b3b98cccb56f2
carlos3g <52337966+carlos3g@users.noreply.github.com>
2021-09-08 16:57:55 -0300

    [WIP][FEAT] new Screen in Progress: Confession

--- src/screens/Confession/index.js
+++ src/screens/Confession/index.js
@@ -1,12 +1,114 @@
-import React from 'react';
-import { Container, Text } from './styles';
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Header } from '@components/';
+import {
+  Container,
+  ConfessionInfo,
+  Content,
+  DislikeIcon,
+  IconButton,
+  InteractionBar,
+  Text,
+  NextConfessionButton,
+  NextConfessionIcon,
+  Label,
+  LikeIcon,
+  ViewsIcon,
+  Reactions,
+} from './styles';
 
-function Home() {
+function Confession() {
+  const [showBar, setShowBar] = useState(false);
+  const { goBack } = useNavigation();
+
+  const onScrollEndDrag = ({ nativeEvent }) => {
+    // if scrolled to top and the bar is hided
+    if (nativeEvent.velocity.y > 0 && showBar) {
+      setShowBar(false);
+    }
+
+    // if scrolled to bottom and the bar is shown
+    if (nativeEvent.velocity.y < 0 && !showBar) {
+      setShowBar(true);
+    }
+  };
+
   return (
     <Container>
-      <Text>Confession</Text>
+      <Header title="Lorem Ipsum" onBackward={goBack} dismiss={false} />
+
+      <Content onScrollEndDrag={onScrollEndDrag}>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed
+          libero risus. Donec feugiat felis vel leo lacinia dictum. In quis
+          vestibulum urna. Integer eget ex augue. Proin elit magna, egestas eget
+          lacinia quis, scelerisque sit amet risus. Ut tempor sapien id est
+          fermentum, nec tristique nisl sollicitudin. Aliquam turpis sapien,
+          cursus a ante a, gravida convallis urna. {'\n\n'}
+          Nunc tempor dolor vitae accumsan lacinia. Nunc a odio quis ligula
+          blandit tristique at et enim. Proin fermentum lorem sodales
+          condimentum pellentesque. Morbi hendrerit volutpat efficitur. Cras
+          dolor arcu, interdum eu finibus vel, consectetur sodales diam. Etiam
+          volutpat pulvinar imperdiet. Nullam felis libero, tempor in arcu eget,
+          sagittis mollis nibh. Nulla fringilla maximus dui, ut varius tellus
+          molestie non. Duis aliquet, risus vitae euismod imperdiet. {'\n\n'}
+          Morbi hendrerit volutpat efficitur. Cras dolor arcu, interdum eu
+          finibus vel, consectetur sodales diam. Etiam volutpat pulvinar
+          imperdiet. Nullam felis libero, tempor in arcu eget, sagittis mollis
+          nibh. Nulla fringilla maximus dui, ut varius tellus molestie non. Duis
+          aliquet, risus vitae euismod imperdiet.{'\n\n'}
+          Nunc tempor dolor vitae accumsan lacinia. Nunc a odio quis ligula
+          blandit tristique at et enim. Proin fermentum lorem sodales
+          condimentum pellentesque. Morbi hendrerit volutpat efficitur. Cras
+          dolor arcu, interdum eu finibus vel, consectetur sodales diam. Etiam
+          volutpat pulvinar imperdiet. Nullam felis libero, tempor in arcu eget,
+          sagittis mollis nibh. Nulla fringilla maximus dui, ut varius tellus
+          molestie non. Duis aliquet, risus vitae euismod imperdiet.
+          {'\n\n'}
+          Nunc tempor dolor vitae accumsan lacinia. Nunc a odio quis ligula
+          blandit tristique at et enim. Proin fermentum lorem sodales
+          condimentum pellentesque. Morbi hendrerit volutpat efficitur. Cras
+          dolor arcu, interdum eu finibus vel, consectetur sodales diam. Etiam
+          volutpat pulvinar imperdiet. Nullam felis libero, tempor in arcu eget,
+          sagittis mollis nibh. Nulla fringilla maximus dui, ut varius tellus
+          molestie non. Duis aliquet, risus vitae euismod imperdiet. {'\n\n'}
+          Morbi hendrerit volutpat efficitur. Cras dolor arcu, interdum eu
+          finibus vel, consectetur sodales diam. Etiam volutpat pulvinar
+          imperdiet. Nullam felis libero, tempor in arcu eget, sagittis mollis
+          nibh. Nulla fringilla maximus dui, ut varius tellus molestie non. Duis
+          aliquet, risus vitae euismod imperdiet.{'\n\n'}
+          Nunc tempor dolor vitae accumsan lacinia. Nunc a odio quis ligula
+          blandit tristique at et enim. Proin fermentum lorem sodales
+          condimentum pellentesque. Morbi hendrerit volutpat efficitur. Cras
+          dolor arcu, interdum eu finibus vel, consectetur sodales diam. Etiam
+          volutpat pulvinar imperdiet. Nullam felis libero, tempor in arcu eget,
+          sagittis mollis nibh. Nulla fringilla maximus dui, ut varius tellus
+          molestie non. Duis aliquet, risus vitae euismod imperdiet.
+        </Text>
+      </Content>
+
+      <InteractionBar dismiss={showBar}>
+        <Reactions>
+          <IconButton>
+            <LikeIcon />
+            <Label>25</Label>
+          </IconButton>
+          <IconButton style={{ marginLeft: 28 }}>
+            <DislikeIcon />
+            <Label>6</Label>
+          </IconButton>
+        </Reactions>
+        <ConfessionInfo>
+          <ViewsIcon />
+          <Label>423</Label>
+        </ConfessionInfo>
+      </InteractionBar>
+
+      <NextConfessionButton>
+        <NextConfessionIcon />
+      </NextConfessionButton>
     </Container>
   );
 }
 
-export default Home;
+export default Confession;
--- src/screens/Confession/styles.js
+++ src/screens/Confession/styles.js
@@ -1,15 +1,107 @@
 import styled from 'styled-components/native';
+import { MotiView } from 'moti';
+import { Feather, Ionicons } from '@expo/vector-icons';
+import { Dimensions } from 'react-native';
+import { shade, tint } from 'polished';
 import { colors } from '@styles/';
 
+const { height: screenHeight, width: screenWidth } = Dimensions.get('screen');
+
 export const Container = styled.View`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
   background-color: ${colors.background};
+  flex: 1;
 `;
 
-export const Text = styled.Text`
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    flexGrow: 1,
+    padding: 16,
+    paddingTop: 0,
+  },
+})``;
+
+export const Text = styled.Text.attrs({
+  selectable: true,
+  selectionColor: tint(0.2, colors.primary),
+})`
+  color: ${colors.text};
+  font-family: RobotoSlab_400Regular;
+  font-size: 14px;
+`;
+
+export const NextConfessionButton = styled.TouchableOpacity`
+  background-color: ${colors.primary};
+  border-radius: 40px;
+  bottom: ${screenHeight * 0.35}px;
+  height: 80px;
+  justify-content: center;
+  left: ${screenWidth - 28}px;
+  opacity: 0.5;
+  padding-left: 6px;
+  position: absolute;
+  width: 80px;
+`;
+
+export const NextConfessionIcon = styled(Ionicons).attrs({
+  color: colors.text,
+  name: 'chevron-forward',
+  size: 20,
+})``;
+
+export const InteractionBar = styled(MotiView).attrs((props) => ({
+  animate: { bottom: props.dismiss ? -100 : 16 },
+  transition: {
+    type: 'timing',
+    duration: 450,
+  },
+}))`
+  align-items: center;
+  align-self: center;
+  background-color: ${colors.primary};
+  border: 1px solid ${shade(0.1, colors.primary)};
+  border-radius: 12px;
+  flex-direction: row;
+  height: 60px;
+  justify-content: space-between;
+  padding: 0px 24px;
+  position: absolute;
+  width: ${screenWidth - 32}px;
+`;
+
+export const Reactions = styled.View`
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const ConfessionInfo = styled.View`
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const IconButton = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const LikeIcon = styled(Feather).attrs({
+  color: tint(0.1, colors.text),
+  name: 'thumbs-up',
+  size: 20,
+})``;
+
+export const DislikeIcon = styled(LikeIcon).attrs({
+  name: 'thumbs-down',
+})``;
+
+export const ViewsIcon = styled(Ionicons).attrs({
+  color: tint(0.1, colors.text),
+  name: 'eye-outline',
+  size: 28,
+})``;
+
+export const Label = styled.Text`
+  color: ${tint(0.1, colors.text)};
   font-family: RobotoSlab_400Regular;
   font-size: 16px;
-  color: ${colors.text};
+  margin-left: 5px;
 `;
